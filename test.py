1d440478223f5f639f04e1569955043eba8e1aed
light3739 <51126304+light3739@users.noreply.github.com>
2023-11-30 18:31:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,3 +10,23 @@ keyboard.add_hotkey('ctrl+c+c', on_ctrl_c_c_press)
 
 # Keep the script running
 keyboard.wait()
+
+import tkinter as tk
+
+# Create the main window
+root = tk.Tk()
+root.attributes('-topmost', True)  # Keep the window on top of others
+
+# Get the screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Create a white dot
+dot = tk.Label(root, text="●", font=("Arial", 20), fg="white", bg="white")
+dot.place(x=screen_width-20, y=screen_height-20)  # Position the dot in the lower right corner
+
+# After two seconds, remove the dot
+root.after(2000, dot.destroy)
+
+# Run the application
+root.mainloop()
