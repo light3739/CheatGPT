bfb4550eec296a76fe3dc5ee519861f636b723f6
light3739 <51126304+light3739@users.noreply.github.com>
2023-12-11 17:42:01 +0200

    Update script.py
    Adding success window after request

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import openai
 import keyboard
 import pyperclip
@@ -7,10 +8,26 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Initialize the OpenAI client
-client = openai.OpenAI(api_key="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")
+client = openai.OpenAI(api_key="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")
 
 # Initialize an empty list to hold the conversation history
 conversation_history = []
+
+
+def show_window():
+    # Create the main window
+    root = tk.Tk()
+    root.attributes('-topmost', True)  # Keep the window on top of others
+    root.attributes('-alpha', 1)
+    root.overrideredirect(True)  # Remove the window decorations
+    # Set the size and position of the window
+    root.geometry("10x10+0+0")
+    # Set the background color of the window to green
+    root.configure(background='green')
+    # Hide the window after 2 seconds
+    root.after(2000, root.destroy)
+    # Run the application
+    root.mainloop()
 
 
 def chat_with_model(user_input):
@@ -43,6 +60,8 @@ def on_ctrl_c_c_press():
     # Call the chat function
     chat_completion = chat_with_model(selected_text)
     logging.info(chat_completion.choices[0].message.content)
+    # Show the window
+    show_window()
 
 
 # Listen for the CTRL+C+C key combination
